4b210bbfe138956763cbff94843d182c11fd5959
Minseo Kim <gr22nist@gmail.com>
2024-05-23 08:05:51 +0900

    백업

--- 3.python/quiz/quiz_101-130.py
+++ 3.python/quiz/quiz_101-130.py
@@ -95,15 +95,110 @@
 # else:
 #     print(user.lower())
 
-score = input('score: ')
-score = int(score)
-if 81 <= score <= 100:
-    print('grade is A')
-elif 61 <= score <= 80:
-    print('grade is B')
-elif 41 <= score <= 60:
-    print('grade is C')
-elif 21 <= score <= 40:
-    print('grade is D')
-else:
-    print('grade is E')
+#122
+
+# score = input('score: ')
+# score = int(score)
+# if 81 <= score <= 100:
+#     print('grade is A')
+# elif 61 <= score <= 80:
+#     print('grade is B')
+# elif 41 <= score <= 60:
+#     print('grade is C')
+# elif 21 <= score <= 40:
+#     print('grade is D')
+# else:
+#     print('grade is E')
+
+# 123
+
+# exchange = {"달러": 1167, "엔": 1.096, "유로": 1268, "위안": 171}
+# user = input("입력 :")
+# num, currency = user.split()
+# print(float(num) * exchange[currency], "원")
+
+# num1 = input("number1: ")
+# num2 = input("number2: ")
+# num3 = input("number3: ")
+# num1 = int(num1)
+# num2 = int(num2)
+# num3 = int(num3)
+
+# if num1 >= num2 and num1 >= num3:
+#     print(num1)
+# elif num2 >= num1 and num2 >= num3:
+#     print(num2)
+# else:
+#     print(num3)
+    
+# 125
+
+# number = input('휴대폰 번호를 입력하세요: ')
+# num = number.split("-")[0]
+# if num == "011":
+#     com = ("SKT")
+# elif num == "016":
+#     com = ("KT")
+# elif num == "019":
+#     com = ("LGU")
+# else: 
+#     com =("알 수 없음")
+    
+# print(f'당신은 {com} 사용자입니다.')
+
+# 126
+
+# num = input('우편번호: ')
+# num = num[:3]
+# if num in ['010', '011','012']:
+#     print("강북구")
+# elif num in ['013', '014','015']:
+#     print("도봉구")
+# else:
+#     print("노원구")
+
+# 127
+
+# num = input('주민등록번호를 입력하세요: ')
+# nums = num.split("-")[1]
+# if nums[0] == "1" or nums[0] == "3":
+#     print("남자")
+# else: 
+#     print("여자")
+
+# 128
+
+# 주민번호 = input('주민등록번호를 입력하세요: ')
+# 뒷자리 = 주민번호.split("-")[1]
+# if 0 <= int(뒷자리[1:3]) <= 8:
+#     print("서울입니다")
+# else:
+#     print("서울이 아닙니다")
+
+# 129
+
+# num = input('주민등록번호를 입력하세요: ')
+# sum1 = int(num[0]*2) + int(num[1]*3) + int(num[2]*4) + \
+#        int(num[3]*5) + int(num[4]*6) + int(num[5]*7) + \
+#        int(num[7]*8) + int(num[8]*9) + int(num[9]*2) + \
+#        int(num[10]*3) + int(num[11]*4) + int(num[12]*5)
+# sum2 = 11 - (sum1 % 11)
+# sum3 = str(sum2)
+# if num[-1] == sum3[-1]:
+#     print('유효한 주민등록번호입니다')
+# else:
+#     print('유효하지 않은 주민등록번호입니다')
+
+# 130
+
+# import requests
+# btc = requests.get("https://api.bithumb.com/public/ticker/").json()['data']
+
+# 변동폭 = float(btc['max_price']) - float(btc['min_price'])
+# 시가 = float(btc['opening_price'])
+# 최고가 = float(btc['max_price'])
+
+# if 시가 + 변동폭 > 최고가:
+#     print('상승장')
+# else:
+#     print('하락장')
